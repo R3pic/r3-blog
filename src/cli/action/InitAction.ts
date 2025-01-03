572c901e69c54bcea1b6e1efed4dd651313cb05c
R3pic <101osc@naver.com>
2025-01-03 19:37:34 +0900

    feat: config cli option

--- src/cli/action/InitAction.ts
+++ src/cli/action/InitAction.ts
@@ -1,8 +1,32 @@
+import path from 'path';
+import fs from 'fs';
 import { AbstractAction } from './AbstractAction';
+import { isExist } from '@/utils/path';
+import { CONFIG_ALREADY_EXIST, show } from '../ui';
 
 export class InitAction extends AbstractAction {
     handle(): never {
-        console.log('콘피그 파일 생성하기');
+        const configFilePath = path.join(process.cwd(), 'blog.config.ts');
+
+        if (isExist(configFilePath)) {
+            show.errMsg(CONFIG_ALREADY_EXIST);
+            process.exit(1);
+        }
+
+        fs.writeFileSync(configFilePath, configContent, { encoding: 'utf-8', flag: 'w' });
+        show.successMsg('기본 설정 파일이 생성되었습니다.');
         process.exit(0);
     }
 }
+
+const configContent = `
+import type { BlogConfig } from 'r3-blog';
+
+const blogConfig: BlogConfig = {
+  title: 'R3',
+  url: 'https://localhost:3000',
+  postDir: '_posts'
+};
+
+export default blogConfig;
+`.trim();
